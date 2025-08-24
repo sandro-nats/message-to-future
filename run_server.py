ffc34299f704f7685aa5bef4626b98332887b4fb
Sandro Natsvlishvili <natsvlishvilisandro1@gmail.com>
2025-08-24 16:20:11 +0400

    Updated backend for Render deployment

--- run_server.py
+++ run_server.py
@@ -1,5 +1,8 @@
 from app import app
+from scheduler import start_scheduler
+import os
 
 if __name__ == "__main__":
+    start_scheduler()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
